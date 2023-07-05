0b1356c58f885961818d5a3fc9b6f2c978194ada
Sai Rajath <sairajath7@gmail.com>
2023-07-05 15:07:49 +0530

    Insert element in sorted array

--- Array/src/basics/InsertElement.java
+++ Array/src/basics/InsertElement.java
@@ -4,7 +4,19 @@ import java.util.Scanner;
 
 public class InsertElement {
 
-    private static void insertElementAtSpecificPosition(int[] array, int size, int element, int position) {
+    private static void insertElementInSortedArray(int[] array, int size, int element) {
+
+        int i;
+
+        for (i = size - 1; (i >= 0 && array[i] > element); i--) {
+
+            array[i + 1] = array[i];
+        }
+
+        array[i + 1] = element;
+    }
+
+    private static void insertElementInUnsortedArray(int[] array, int size, int element, int position) {
 
         for (int i = size - 1; i >= position; i--) {
 
@@ -19,8 +31,18 @@ public class InsertElement {
         int size = 0;
         int element = 0;
         int position = 0;
+        int choice = 0;
 
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("1. Insert in sorted Array");
+        System.out.println("2. Insert in un-sorted Array");
+        System.out.println("Enter your choice:");
+
+        if (scanner.hasNextInt()) {
+
+            choice = scanner.nextInt();
+        }
 
         System.out.println("Enter the size of the array:");
 
@@ -50,14 +72,21 @@ public class InsertElement {
             element = scanner.nextInt();
         }
 
-        System.out.println("Enter the position at which it should be inserted:");
+        if (1 == choice) {
 
-        if (scanner.hasNextInt()) {
+            insertElementInSortedArray(array, size, element);
 
-            position = scanner.nextInt();
+        } else {
+
+            System.out.println("Enter the position at which it should be inserted:");
+
+            if (scanner.hasNextInt()) {
+
+                position = scanner.nextInt();
+            }
+
+            insertElementInUnsortedArray(array, size, element, position);
         }
-
-        insertElementAtSpecificPosition(array, size, element, position);
 
         System.out.println("Array after inserting " + element + " at position " + position);
 
