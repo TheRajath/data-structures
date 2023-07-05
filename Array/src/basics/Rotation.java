17451bad17ceb8a99b223730ccc38bcd03fbe06a
Sai Rajath <sairajath7@gmail.com>
2023-07-05 09:02:48 +0530

    Add right rotation

--- Array/src/basics/Rotation.java
+++ Array/src/basics/Rotation.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Rotation {
 
-    private static int[] rotateOneByOne(int[] array, int size, int numberOfTimesToRotate) {
+    private static int[] leftRotate(int[] array, int size, int numberOfTimesToRotate) {
 
         int position = 1;
 
@@ -25,9 +25,30 @@ public class Rotation {
         return array;
     }
 
+    private static int[] rightRotate(int[] array, int size, int numberOfTimesToRotate) {
+
+        int arrayLength = size - 1;
+
+        for (int position = 0; position < numberOfTimesToRotate; position++) {
+
+            int last = array[arrayLength];
+
+            for (int i = arrayLength; i > 0; i--) {
+
+                array[i] = array[i - 1];
+            }
+
+            array[0] = last;
+
+        }
+
+        return array;
+    }
+
     public static void main(String[] args) {
 
         int size = 0;
+        int choice = 0;
         int numberOfTimesToRotate = 0;
 
         Scanner scanner = new Scanner(System.in);
@@ -58,7 +79,22 @@ public class Rotation {
             numberOfTimesToRotate = scanner.nextInt();
         }
 
-        int[] rotatedArray = rotateOneByOne(array, size, numberOfTimesToRotate);
+        System.out.println("1. Left Rotate");
+        System.out.println("2. Right Rotate");
+        System.out.println("Enter your choice:");
+
+        if (scanner.hasNextInt()) {
+
+            choice = scanner.nextInt();
+        }
+
+        int[] rotatedArray = new int[100];
+
+        switch (choice) {
+            case 1 -> rotatedArray = leftRotate(array, size, numberOfTimesToRotate);
+            case 2 -> rotatedArray = rightRotate(array, size, numberOfTimesToRotate);
+            default -> System.exit(0);
+        }
 
         System.out.println("Rotated Array:");
 
