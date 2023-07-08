f296276ae837c2b7560f4700e960e5f446562937
Sai Rajath <sairajath7@gmail.com>
2023-07-08 21:31:02 +0530

    Get data from Nth node from the end

---
+++ LinkedList/src/basics/GetNthNodeFromEnd.java
@@ -0,0 +1,53 @@
+package basics;
+
+import java.util.Scanner;
+
+import static basics.InsertNode.insertInitialValues;
+
+public class GetNthNodeFromEnd {
+
+    private static int getNthNodeFromEnd(SingleNode head, int length, int index) {
+
+        SingleNode temp;
+
+        if (length < index)
+            return 0;
+
+        temp = head;
+
+        for (int i = 1; i < length - 1; i++) {
+
+            temp = temp.next;
+        }
+
+        return temp.data;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        CustomLinkedList customLinkedList = insertInitialValues();
+        SingleNode head = customLinkedList.head;
+
+        int index = 0;
+
+        /*
+        Position Starts with 1
+        */
+
+        System.out.println("Get Data of Nth node: ");
+
+        if (scanner.hasNextInt()) {
+
+            index = scanner.nextInt();
+        }
+
+        int length = customLinkedList.length(head);
+
+        int data = getNthNodeFromEnd(head, length, index);
+
+        System.out.println("Data: " + data);
+    }
+
+}
